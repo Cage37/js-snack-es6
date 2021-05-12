059c2a137fc8415075ca41d1f630f2dc43ade6c3
Cage37 <casiraghi.smn@gmail.com>
2021-05-12 16:49:20 +0200

    Task complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,38 +3,38 @@
 // nome e peso.
 // Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
 
-const bycicle = [
-    {
-        bycicleName: "Mountain bike",
-        bycicleWeight: 600
-    },
-    {
-        bycicleName: "City bike",
-        bycicleWeight: 700
-    },
-    {
-        bycicleName: "BMX",
-        bycicleWeight: 400
-    }
-];
-
-// var minWeightBycicle = "";
-
-// for (let i = 0; i < bycicle.length; i++) {
-//     if (minWeightBycicle == "") {
-//         minWeightBycicle = bycicle[i];
-//     } else if (minWeightBycicle.bycicleWeight > bycicle[i].bycicleWeight) {
-//         minWeightBycicle = bycicle[i];
+// const bycicle = [
+//     {
+//         bycicleName: "Mountain bike",
+//         bycicleWeight: 600
+//     },
+//     {
+//         bycicleName: "City bike",
+//         bycicleWeight: 700
+//     },
+//     {
+//         bycicleName: "BMX",
+//         bycicleWeight: 400
 //     }
-// }
+// ];
 
-// console.log(minWeightBycicle);
+// // var minWeightBycicle = "";
 
-const [ , , bycicleThree] = bycicle;
-const {bycicleName, bycicleWeight} = bycicleThree;
-console.log(bycicleThree);
+// // for (let i = 0; i < bycicle.length; i++) {
+// //     if (minWeightBycicle == "") {
+// //         minWeightBycicle = bycicle[i];
+// //     } else if (minWeightBycicle.bycicleWeight > bycicle[i].bycicleWeight) {
+// //         minWeightBycicle = bycicle[i];
+// //     }
+// // }
 
-document.getElementById("bycicle").innerHTML = `LA BICICLETTA CHE PESA MENO E': ${bycicleName} e pesa ${bycicleWeight}.`;
+// // console.log(minWeightBycicle);
+
+// const [ , , bycicleThree] = bycicle;
+// const {bycicleName, bycicleWeight} = bycicleThree;
+// console.log(bycicleThree);
+
+// document.getElementById("bycicle").innerHTML = `LA BICICLETTA CHE PESA MENO E': ${bycicleName} e pesa ${bycicleWeight}.`;
 
 
 
@@ -44,56 +44,94 @@ document.getElementById("bycicle").innerHTML = `LA BICICLETTA CHE PESA MENO E': 
 // Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
 // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
-const team = [
-    {   
-        teamName: "Inter",
-        pointsMade: 0,
-        foulsSuffered: 0
-    },
-    {   
-        teamName: "Milan",
-        pointsMade: 0,
-        foulsSuffered: 0
-    },
-    {   
-        teamName: "Juventus",
-        pointsMade: 0,
-        foulsSuffered: 0
-    },
-    {   
-        teamName: "Roma",
-        pointsMade: 0,
-        foulsSuffered: 0
-    },
-    {   
-        teamName: "Lazio",
-        pointsMade: 0,
-        foulsSuffered: 0
-    },
-    {   
-        teamName: "Napoli",
-        pointsMade: 0,
-        foulsSuffered: 0
-    },
-    {   
-        teamName: "Atalanta",
-        pointsMade: 0,
-        foulsSuffered: 0
-    }
-];
+// const team = [
+//     {   
+//         teamName: "Inter",
+//         pointsMade: 0,
+//         foulsSuffered: 0
+//     },
+//     {   
+//         teamName: "Milan",
+//         pointsMade: 0,
+//         foulsSuffered: 0
+//     },
+//     {   
+//         teamName: "Juventus",
+//         pointsMade: 0,
+//         foulsSuffered: 0
+//     },
+//     {   
+//         teamName: "Roma",
+//         pointsMade: 0,
+//         foulsSuffered: 0
+//     },
+//     {   
+//         teamName: "Lazio",
+//         pointsMade: 0,
+//         foulsSuffered: 0
+//     },
+//     {   
+//         teamName: "Napoli",
+//         pointsMade: 0,
+//         foulsSuffered: 0
+//     },
+//     {   
+//         teamName: "Atalanta",
+//         pointsMade: 0,
+//         foulsSuffered: 0
+//     }
+// ];
 
-const newTeams = [];
+// const newTeams = [];
 
-for (let i = 0; i < team.length; i++) {
-    team[i].pointsMade = Math.floor(Math.random() * 101);
-    team[i].foulsSuffered = Math.floor(Math.random() * 501);
-    const {teamName, foulsSuffered} = team[i];
-    const newTeam = {
-        teamName,
-        foulsSuffered
-    }
-    newTeams.push(newTeam);
-};
+// for (let i = 0; i < team.length; i++) {
+//     team[i].pointsMade = Math.floor(Math.random() * 101);
+//     team[i].foulsSuffered = Math.floor(Math.random() * 501);
+//     const {teamName, foulsSuffered} = team[i];
+//     const newTeam = {
+//         teamName,
+//         foulsSuffered
+//     }
+//     newTeams.push(newTeam);
+// };
 
-console.log(newTeams);
+// console.log(newTeams);
 
+
+// Snack 3:
+// Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+
+
+// METODO FILTER
+
+// function arrayNumbers (array, a, b) {
+//     const newArray = array.filter ((numero) => {
+//         if(numero > a && numero < b) {
+//             return true;
+//         }
+//         return false;
+//     })
+//         return newArray;
+// };
+
+// const arrayNumb = [1,2,3,4,5,6,7,8,9,10]
+// console.log(arrayNumbers(arrayNumb, 3, 7));
+
+
+// METODO FOREACH
+
+function arrayNumbers (array, a, b) {
+    const newNumber = [];
+    array.forEach((element, i, array) => {
+        if (element > a && element < b) {
+            newNumber.push(element)
+        }
+    });
+
+    return newNumber;
+}
+
+const arrayNumb = [1,2,3,4,5,6,7,8,9,10]
+console.log(arrayNumbers(arrayNumb, 3, 7));
+    
